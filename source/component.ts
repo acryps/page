3858184bdbbd918a5c0adacd689413cc1e303818
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2023-12-13 14:02:07 +0100

    Add onerror default error to console output

--- source/component.ts
+++ source/component.ts
@@ -25,7 +25,10 @@ export class Component {
 	
 	onload(): Promise<void> | void {}
 	onrouteleave(): Promise<void> | void {}
-	onerror(error): Promise<void> | void {}
+	
+	onerror(error): Promise<void> | void {
+		console.error(`Error occured in '${this.constructor.name}'`, error);
+	}
 
 	onchildchange(parameters, route: Route, component: Component): Promise<void> | void {}
 
